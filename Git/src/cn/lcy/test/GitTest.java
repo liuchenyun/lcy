6ab68402c789b01487016de6e44be0d55fec54c6
liuchenyun <1210737588@qq.com>
2018-09-04 20:50:24 +0800

    aaa

--- Git/src/cn/lcy/test/GitTest.java
+++ Git/src/cn/lcy/test/GitTest.java
@@ -1,10 +1,11 @@
-package cn.lcy.test;
+﻿package cn.lcy.test;
 
 public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("git第一次");
-
+		
+		System.out.println("aaa");
 	}
 
 }
